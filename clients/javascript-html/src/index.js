9174036e1c1573647dd07c9a74783b0f36079faf
Nick Fisher <nick.fisher@avinium.com>
2019-04-14 13:34:15 +1000

    add javascript client

---
+++ clients/javascript-html/src/index.js
@@ -0,0 +1,20 @@
+import { EventApi, EventRequestBody } from '../../../dashboard/lib/api';
+
+export const SetApiKey = (apiKey) => {
+  window.SWEEP_API_KEY = apiKey;
+} 
+
+export const Raise = (eventName, eventParams) => {
+    if(window.SWEEP_API_KEY == null) 
+      throw "Sweep API key not set";
+
+    console.log(window.SWEEP_API_KEY);
+  
+      let requestBody = {
+        eventName: eventName,
+        params:eventParams
+      }
+      return new EventApi().addEvent(requestBody, window.SWEEP_API_KEY, {}).then((foo) => {
+        console.log("FOO");
+      }).catch((err) => {console.error(err)});
+} 
---
+++ clients/javascript-html/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'Sweep',
+  },
+  mode:"development",
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+        }
+    ]
+  },
+};
