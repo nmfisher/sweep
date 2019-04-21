f085f2c7da7c5cefedbce30169b44eb9d57e6c01
Nick Fisher <nick.fisher@avinium.com>
2019-04-21 23:15:03 +1000

    add build scripts

---
+++ build_production.sh
@@ -0,0 +1,2 @@
+./generate_all_from_spec.sh production
+dotnet publish -c Release server/Sweep/src -o ../../../publish
--- generate_clients_from_spec.sh
+++ generate_clients_from_spec.sh
@@ -1,6 +1,9 @@
+spec_file=sweep.yaml
+
 shopt -s nocasematch
 case "production" in
  $1 ) cp sweep.yaml sweep_prod.yaml; spec_file=sweep_prod.yaml; sed -i -e 's/sweep-development\.ngrok\.io/app\.sweephq\.com/g' sweep_prod.yaml;;
 esac
 
-java -jar $GENERATOR_PATH generate -i $spec_file -g dart -o ./clients/lib/dart -DpackageName=Sweep
+java -jar $GENERATOR_PATH generate -i $spec_file -g dart -o ./clients/lib/dart -t C:\\Users\\nickh\\Documents\\Projects\\openapi-generator\\modules\\openapi-generator\\src\\main\\resources\\dart2 -DpackageName=Sweep -DpubName=sweep_api -DbrowserClient=false -DdebugModels 
+java -jar $GENERATOR_PATH generate -i $spec_file -g csharp-netcore -o ./clients/lib/dotnet -DpackageName=Sweep
